a5d843a8b0cef792f192c2d5209a51e9a56b408f
famchankou <famchankou@gmail.com>
2024-05-20 09:59:04 +0200

    chore: add grid example

--- src/canvas-examples/main.js
+++ src/canvas-examples/main.js
@@ -52,9 +52,13 @@ class FlowFieldEffect {
     this.lastTime = 0;
     this.interval = 1000 / 60; // 60fps
     this.timer = 0;
+    this.cellSize = 15;
+    this.gradient;
+    this.#createGradient();
+    this.#ctx.strokeStyle = this.gradient;
   }
 
-  #draw(x, y) {
+  #drawLine(x, y) {
     const length = 300;
 
     this.#ctx.beginPath();
@@ -65,18 +69,29 @@ class FlowFieldEffect {
     this.#ctx.stroke();
   }
 
+  #createGradient() {
+    this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height);
+    this.gradient.addColorStop('0.1', '#ff5c33');
+    this.gradient.addColorStop('0.2', '#ff66b2');
+    this.gradient.addColorStop('0.4', '#ccccff');
+    this.gradient.addColorStop('0.6', '#b3ffff');
+    this.gradient.addColorStop('0.9', '#80ff80');
+    this.gradient.addColorStop('0.9', '#ffff33');
+  }
+
   animate(timeStamp) {
     // measuring delta time for faster animation serving
     let deltaTime = timeStamp - this.lastTime;
     this.lastTime = timeStamp;
 
     if (this.timer > this.interval) {
-      // this.#draw(this.x, this.y);
+      // this.#drawLine(this.x, this.y);
       // start from the center
-      // this.#draw(this.#width / 2, this.#height / 2);
+      // this.#drawLine(this.#width / 2, this.#height / 2);
       this.#ctx.clearRect(0, 0, this.#width, this.#height);
       // make circular movements
-      this.#draw(this.#width / 2 + Math.sin(this.angle) * 100, this.#height / 2 + Math.cos(this.angle) * 100);
+      this.#drawLine(this.#width / 2 + Math.sin(this.angle) * 100, this.#height / 2 + Math.cos(this.angle) * 100);
+      this.timer = 0;
     } else {
       this.timer += deltaTime;
     }
@@ -85,6 +100,27 @@ class FlowFieldEffect {
     this.angle += 0.1;
 
     console.log('animating');
+    flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  drawGrid(timeStamp) {
+    // measuring delta time for faster animation serving
+    let deltaTime = timeStamp - this.lastTime;
+    this.lastTime = timeStamp;
+
+    if (this.timer > this.interval) {
+      this.#ctx.clearRect(0, 0, this.#width, this.#height);
+
+      for (let y = 0; y < this.#height; y += this.cellSize) {
+        for (let x = 0; x < this.#width; x += this.cellSize) {
+          this.#drawLine(x, y);
+        }
+      }
+
+      this.timer = 0;
+    } else {
+      this.timer += deltaTime;
+    }
     flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
   }
 }
