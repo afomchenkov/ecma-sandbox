c9b045e716e0e843f542a294e5b02c7157a82e59
Aliaksandr Famchankou <afamchankou@gmail.com>
2024-11-30 12:13:58 +0100

    chore: add task find word

--- src/task/backtracking.js
+++ src/task/backtracking.js
@@ -49,3 +49,147 @@ const generateParenBacktracking = (n) => {
 
   return result;
 };
+
+// implementation 1
+function findWord1(board, word) {
+  const ROWS = board.length;
+  const COLS = board[0].length;
+
+  const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+
+  function backtrack(row, col, suffix) {
+    if (suffix.length == 0) return true;
+    if (
+      row < 0 ||
+      row == ROWS ||
+      col < 0 ||
+      col == COLS ||
+      board[row][col] != suffix.charAt(0)
+    ) {
+      return false;
+    }
+
+    let ret = false;
+    // mark the path before the next exploration
+    board[row][col] = "#";
+    for (let [rowOffset, colOffset] of directions) {
+      ret = backtrack(row + rowOffset, col + colOffset, suffix.slice(1));
+      if (ret) {
+        break;
+      }
+    }
+    // backtrack
+    board[row][col] = suffix.charAt(0);
+    return ret;
+  }
+
+  for (let row = 0; row < ROWS; ++row) {
+    for (let col = 0; col < COLS; ++col) {
+      if (backtrack(row, col, word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// implementation 2
+function findWord2(board, word) {
+  const ROWS = board.length;
+  const COLS = board[0].length;
+  const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+
+  function backtrack(row, col, suffix) {
+    if (suffix.length == 0) return true;
+    if (
+      row < 0 ||
+      row == ROWS ||
+      col < 0 ||
+      col == COLS ||
+      board[row][col] != suffix.charAt(0)
+    ) {
+      return false;
+    }
+
+    // mark the path before the next exploration
+    board[row][col] = "#";
+    for (let [rowOffset, colOffset] of directions) {
+      if (backtrack(row + rowOffset, col + colOffset, suffix.slice(1))) {
+        // return without cleanup
+        return true;
+      }
+    }
+
+    // backtrack
+    board[row][col] = suffix.charAt(0);
+    return false;
+  }
+
+  for (let row = 0; row < ROWS; ++row) {
+    for (let col = 0; col < COLS; ++col) {
+      if (backtrack(row, col, word)) return true;
+    }
+  }
+  return false;
+}
+
+// implementation 3
+function findWord3(board, word) {
+  let rows = board.length;
+  let cols = board[0].length;
+  let isFound = false;
+
+  function dfs(i, j, charIndex) {
+    // check boundaries
+    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] == -1) {
+      return;
+    }
+
+    if (board[i][j] != word.charAt(charIndex)) {
+      return;
+    }
+
+    if (charIndex >= word.length - 1) {
+      // this means the word is found
+      isFound = true;
+      return;
+    }
+
+    const letter = board[i][j];
+    // mark as visited
+    board[i][j] = -1;
+
+    charIndex += 1;
+    dfs(i + 1, j, charIndex);
+    dfs(i - 1, j, charIndex);
+    dfs(i, j + 1, charIndex);
+    dfs(i, j - 1, charIndex);
+
+    // backtrack
+    board[i][j] = letter;
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] == word.charAt(0)) {
+        dfs(i, j, 0);
+
+        if (isFound) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
