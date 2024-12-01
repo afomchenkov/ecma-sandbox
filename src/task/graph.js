44df5f0f8440e404a574cf09c2dfe588587f581c
AFamchankou <famchankou@gmail.com>
2024-12-01 13:41:51 +0100

    feat: add kahn algorithm

--- src/task/graph.js
+++ src/task/graph.js
@@ -14,13 +14,13 @@ function dijkstraMinPath(edges, start, end) {
 
     while (queue.length) {
         let [cost, path, v1] = queue.pop(); // next min weight
-        
+
         if (!marked.has(v1)) {
             marked.add(v1);
 
             path = [...path, v1];
             if (v1 === end) {
-              return [cost, path];
+                return [cost, path];
             }
 
             for (const { to, distance } of graph.get(v1)) {
@@ -38,17 +38,79 @@ function dijkstraMinPath(edges, start, end) {
 }
 
 const edges = [
-  ["A", "B", 7],
-  ["A", "D", 5],
-  ["B", "C", 8],
-  ["B", "D", 9],
-  ["B", "E", 7],
-  ["C", "E", 5],
-  ["D", "E", 7],
-  ["D", "F", 6],
-  ["E", "F", 8],
-  ["E", "G", 9],
-  ["F", "G", 11],
+    ["A", "B", 7],
+    ["A", "D", 5],
+    ["B", "C", 8],
+    ["B", "D", 9],
+    ["B", "E", 7],
+    ["C", "E", 5],
+    ["D", "E", 7],
+    ["D", "F", 6],
+    ["E", "F", 8],
+    ["E", "G", 9],
+    ["F", "G", 11],
 ];
 
 // console.log(JSON.stringify(dijkstraMinPath(edges, 'A', 'G')));
+
+// Kahn’s algorithm for topological sorting
+// Topological sorted order: It is a linear ordering of vertices such that for
+// every directed edge u -> v, where vertex u comes before v in the ordering.
+
+// Example:
+// Input: V=6 , E = {{2,3}, {3,1}, {4,0}, {4,1}, {5,0}, {5,2}}
+// Output: 4 5 2 0 3 1
+//
+// Input: V=5 , E={{0,1}, {1,2}, {3,2}, {3,4}}
+// Output: 0 3 4 1 2
+
+// Kahn’s Algorithm for Topological Sorting is a method used to order the vertices of a directed
+// graph in a linear order such that for every directed edge from vertex A to vertex B, A comes
+// before B in the order. The algorithm works by repeatedly finding vertices with no incoming edges,
+// removing them from the graph, and updating the incoming edges of the remaining vertices.
+// This process continues until all vertices have been ordered.
+// Function to return list containing vertices in Topological order.
+function sortGraph(n, edges) {
+    function topologicalSort(adj, edgesCount) {
+        const indegree = new Array(edgesCount).fill(0);
+        for (let i = 0; i < edgesCount; i++) {
+            for (const vertex of adj[i]) {
+                indegree[vertex]++;
+            }
+        }
+
+        const q = [];
+        for (let i = 0; i < edgesCount; i++) {
+            if (indegree[i] === 0) {
+                q.push(i);
+            }
+        }
+
+        const result = [];
+        while (q.length > 0) {
+            const node = q.shift();
+            result.push(node);
+
+            for (const adjacent of adj[node]) {
+                indegree[adjacent]--;
+                if (indegree[adjacent] === 0) {
+                    q.push(adjacent);
+                }
+            }
+        }
+
+        if (result.length !== edgesCount) {
+            console.log("Graph contains cycle!");
+            return [];
+        }
+        return result;
+    }
+
+    const adjacencyList = Array.from({ length: n }, () => []);
+    for (const edge of edges) {
+        adjacencyList[edge[0]].push(edge[1]);
+    }
+
+    return topologicalSort(adjacencyList, n);
+}
+console.log('Topological sort: ', sortGraph(6, [[0, 1], [1, 2], [2, 3], [4, 5], [5, 1], [5, 2]]));
