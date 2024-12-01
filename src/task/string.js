c24ddb1b7c1c58509140ea6e2263c1533790f7b8
Aliaksandr Famchankou <afamchankou@gmail.com>
2024-12-01 11:07:13 +0100

    chore: add tasks

--- src/task/string.js
+++ src/task/string.js
@@ -20,7 +20,63 @@ function wordBreak(s, wordDict) {
       }
     }
   }
-  
+
   return false;
 }
-console.log(wordBreak("applepenapple", ["apple", "pen"]));
+// console.log(wordBreak("applepenapple", ["apple", "pen"]));
+
+function minRemoveToMakeValid(s) {
+  const str = s.split("");
+  const st = [];
+  const toRemove = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "(") {
+      st.push({ br: "(", i });
+    }
+    if (str[i] == ")") {
+      if (st.length == 0) {
+        toRemove.add(i);
+        continue;
+      }
+      st.pop();
+    }
+  }
+  for (const r of st) {
+    toRemove.add(r.i);
+  }
+  let res = "";
+  for (let j = 0; j < str.length; j++) {
+    if (!toRemove.has(j)) {
+      res += str[j];
+    }
+  }
+
+  return res;
+}
+
+function minRemoveToMakeValid(s) {
+  let indexesToRemove = new Set();
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) == "(") {
+      stack.push(i);
+    }
+    if (s.charAt(i) == ")") {
+      if (!stack.length) {
+        indexesToRemove.add(i);
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  while (stack.length) indexesToRemove.add(stack.pop());
+  let sb = "";
+  for (let i = 0; i < s.length; i++) {
+    if (!indexesToRemove.has(i)) {
+      sb += s.charAt(i);
+    }
+  }
+  return sb.toString();
+}
